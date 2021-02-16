771c9f7113917e93e3195f7978c773c8e012c6c9
kate <katy.madden99@gmail.com>
2021-02-16 09:03:18 +0000

    Added in rest of the markdown text to be passed to the fs function

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,7 +1,6 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
+// Function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  console.log("here badge");
   switch(license) {
     case "MIT":
       return "![MIT license](https://img.shields.io/badge/License-MIT-blue.svg)";
@@ -15,24 +14,23 @@ function renderLicenseBadge(license) {
   }
 }
 
-// TODO: Create a function that returns the license link
+// Function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-  console.log("here link");
   switch(license) {
     case "MIT":
-      return "\n\nThis project was created under the standard MIT licence.\n\n[Learn more about this licence.](https://lbesson.mit-license.org/)";
+      return "\n\n> This project was created under the standard MIT licence.\n\n[Learn more about this licence.](https://lbesson.mit-license.org/)";
     case "Creative Commons":
-      return "\n\nThis project was created under the Creative Commons licence.\n\n[Learn more about this licence.](https://creativecommons.org/licenses/by-nd/4.0)";
+      return "\n\n> This project was created under the Creative Commons licence.\n\n[Learn more about this licence.](https://creativecommons.org/licenses/by-nd/4.0)";
     case "GPL":
-     return "\n\nThis project was created under the standard GPL licence.\n\n[Learn more about this licence.](http://perso.crans.org/besson/LICENSE.html)";     
+     return "\n\n> This project was created under the standard GPL licence.\n\n[Learn more about this licence.](http://perso.crans.org/besson/LICENSE.html)";     
     default:
       "";
     break;
   }
 }
 
-// TODO: Create a function that returns the license section of README
+// Function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
   if (license !== "Other") {
@@ -44,12 +42,43 @@ function renderLicenseSection(license) {
   }
 }
 
-// TODO: Create a function to generate markdown for README
+// Function to generate markdown for README including all the sections required.
 function generateMarkdown(data) {
   return `# ${data.title}
   ${renderLicenseSection(data.license)}
+  
+  ## Description
+  ${data.description}
+
+  ## Contents
+  Section | Description
+  ------------ | -------------
+  [Installation](#installation) | Information on how to install this project.
+  [Usage](#usage) | Find out how to use the ${data.title}.
+  [Testing](#testing) | All you need to know about testing.
+  [Contribution](#contribution) | Want to contribute? Find out more in this section.
+  [Questions](#questions) | If you've got any questions, this is how you can reach me.
+  
+  ## Installation
+  ${data.installation}
+
+  ## Usage
+  ${data.usage}
+
+  ## Testing
+  ${data.testing}
+
+  ## Contribution
+  ${data.contributors}
+
+  ## Questions
+  My GitHub profile, ${data.github} can be found [here](${data.githubLink}).
+
+  Otherwise, if you have any questions, you can reach me directly at [${data.email}](mailto:${data.email}).
+
 
 `;
 }
 
+//Allows other files to use the information generated here.
 module.exports = generateMarkdown;
